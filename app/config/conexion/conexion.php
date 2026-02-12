05c5900f1c4eeafbc0d42a5cd84d606e9c41281d
Albertomovil2017 <albertoluisrodriguez90@email.com>
2026-02-12 08:31:23 +0100

    Cambios de archivo de conexión

--- app/config/conexion/conexion.php
+++ app/config/conexion/conexion.php
@@ -8,18 +8,13 @@ $db_name = $env['DB_NAME'];
 $user = $env['DB_USER'];
 $password = $env['DB_PASS'];
 
-    //Ejecutamos la estructura try para intentar ejecutar la conexión a la bd , sino nos muestra un error con el catch
-    try {
-        //Creamos la conexión segura con pdo a la bd, siendo mucho más seguro y evitando sqlInjection
-        $pdo = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8mb4", $user, $password, [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        ]);
+        
+    $conexion = mysqli_connect($host, $user, $password, $db_name);
 
-    } catch (PDOException $e) {
-
-        echo "Error de conexion" . $e;
-
-    };
+    
+    if(!$conexion) {
+        echo "Error de conexión: " . mysqli_connect_error();
+    }
 
 
 
