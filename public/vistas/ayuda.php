5b7c9c96731903e41783c3da677b3f30bd5da85f
Lorenzo7112 <arj101077@gmail.com>
2026-03-03 00:14:05 +0100

    Le quité lo de las opciones de credito o equipamiento

--- public/vistas/ayuda.php
+++ public/vistas/ayuda.php
@@ -2,12 +2,12 @@
 include_once 'header.php';
 ?>
 
-<section class="w-full  mx-auto p-10 flex flex-col items-center lg:min-h-150  justify-center">
+<section class="w-full  mx-auto p-10 flex flex-col items-center lg:min-h-150  justify-center gap-10 ">
     
-<!--Logo-->
-  <div class="w-16 h-16 rounded-full bg-white flex items-center justify-center shadow-md z-10">
-    <img src="../assets/hoja.png" alt="Logo" class="w-10 h-10">
-  </div>
+    <!--Logo-->
+    <div class="w-16 h-16 rounded-full bg-gray-100 flex items-center justify-center">
+        <img src="../assets/hoja.png" alt="hoja" class="inline w-6 h-6 md:w-8 md:h-8">
+    </div>
 
     <div class="w-200 h-175 border border-gray-300 rounded-lg shadow-lg flex flex-col items-center justify-center gap-20"
         style="border: 2px solid; 
@@ -16,25 +16,17 @@ include_once 'header.php';
     <h3 class="text-4xl font-bold bg-gradient-to-l from-green-500 to-green-800 text-transparent bg-clip-text">Solicitar Ayuda</h3>
 
     <form action="#" method="post" class="w-120 flex flex-col items-center h-100 gap-20">
-        
-    <!--Título tipo de ayuda-->
-        <h4 class="text-2xl font-bold ">Tipo de ayuda</h4>
-        <!--Tipo de ayuda-->
-        
-        <select name="tipo" id="tipo" class="w-full h-100 text-center border border-gray-300 rounded-lg shadow-lg"
-        style="border: 2px solid; 
-            border-image: linear-gradient(to left, #22c55e, #166534) 1;">
-            <option value="credito">Crédito</option>
-            <option value="equipo">Equipo</option>
-        </select>
-        
-
-        <!--Mnesaje-->
+    
+        <h4 class="text-4xl font-bold bg-black bg-clip-text">Cuentenos,lo que usted necesita es...</h4>
+        <!--Mensaje-->
             <textarea name="textoSolicitud" id="textoSolicitud" placeholder="Escriba su mensaje aquí..." class="w-full h-50 rounded-lg p-2 resize-none"  style="
             border: 2px solid;
             border-image: linear-gradient(to left, #22c55e, #166534) 1;"></textarea>
-            <button type="submit" class="w-full bg-gradient-to-l from-green-500 to-green-800 hover:bg-gradient-to-l hover:from-green-600 hover:to-green-900 text-white font-medium py-3 rounded-lg transition text-center">Enviar solicitud</button>
-        
+            <button type="submit" class="w-full bg-gradient-to-l from-green-500 to-green-800 hover:bg-gradient-to-l hover:from-green-600 hover:to-green-900 text-white font-medium py-3 rounded-lg transition text-center " name="solicitar">Enviar solicitud</button>
+
     </form>
     </div>
+
+
+
 </section>
